595418588eb1134f9fe7130d845aa26203fa0176
chandareth <chhay.daret@gmail.com>
2025-07-20 14:03:38 +0700

    Upload Entire Project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './index.css'
+
+function App() {
+
+
+  return (
+    <h1 className="text-2xl/7 font-bold text-blue-900 sm:truncate sm:text-3xl sm:tracking-tight">
+        React + Typescript + Vite + TailwindCss
+    </h1>
+  )
+}
+
+export default App
